22878d79cfcc0f68edd45e4015f3a39cc8f67b40
craig clark <lumberjackchester@gmail.com>
2017-01-30 21:03:25 -0500

    initializing site

---
+++ admin/access.php
@@ -0,0 +1,13 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'On');
+ini_set('date.timezone', 'America/New_York');
+
+include_once realpath(__DIR__ . '/../..') . '/config/config.php';

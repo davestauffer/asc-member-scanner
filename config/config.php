cd39a820a49441a9fd66c7dd5038b0fc7f23339d
craig clark <lumberjackchester@gmail.com>
2017-02-12 13:03:55 -0500

    moving assets to /admin  directory

---
+++ config/config.php
@@ -0,0 +1,20 @@
+
+<?php
+
+define('DBNAME', 'archstreetcenterscanner');
+define('DBUSER', 'dbudevdata');
+define('DBPASS', 'QFoSMG5SNpe0peLd');
+define('DBHOST', 'localhost');
+define('DBCHAR', 'utf8');
+define('VHROOT', 'D:\creg docs\Git Projects\PHP Arch Street\admin');
+//define('DOCSROOT', VHROOT . '/docs');
+define('WSROOT', VHROOT . '/httpdocs');
+define('ERRORLOGS', WSROOT . '/logs');
+define('SGROOT', WSROOT . '/php');
+define('WSPATH', '');
+define('CKNAME', 'xsession');
+define('SMTPPASS', 'IKDQhjo]9u7Rbv|W');
+define('SMTPEMAIL', 'noreply@efundsolutions.net');
+define('EMAILNAME', 'Efunding Solutions');
+define('SERVERRURL', 'http://' . $_SERVER['SERVER_NAME'] . WSPATH);
+define('SYSTEMUID', 1);

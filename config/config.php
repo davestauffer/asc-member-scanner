b5b9e968c4f95ed8d870416f6edbe884b1b50c5d
craig clark <lumberjackchester@gmail.com>
2017-06-08 10:35:01 -0400

    renaming files adding search class and temp index file

--- config/config.php
+++ config/config.php
@@ -1,6 +1,11 @@
 
 <?php
 
+ini_set('error_reporting', E_ALL);
+ini_set('display_errors', 'On');
+ini_set('date.timezone', 'America/New_York');
+
+
 define('DBNAME', 'archstreetcenterscanner');
 define('DBUSER', 'dbudevdata');
 define('DBPASS', 'QFoSMG5SNpe0peLd');
